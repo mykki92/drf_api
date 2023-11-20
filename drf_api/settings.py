082ca23ec0cbc5364441c9d61d575bfc778c40da
mykki92 <mike2108@hotmail.co.uk>
2023-11-20 13:25:23 +0000

    deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,8 +48,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
-# DEBUG = False
+# DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-mykki92-drfapi-erdcd6s9hgt.ws-eu106.gitpod.io',
